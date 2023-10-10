ef60893b08f40869ee4a04ba5147e121d39bb691
Aleksandr Stepanenko <alalstepan@gmail.com>
2023-10-11 00:26:34 +1200

    feature: выполена часть задания - string, fibonacci, sorting, stack

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { useFormAndValidation } from "../../hooks/useForm";
+import { ElementStates } from "../../types/element-states";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Stack } from "./stack";
+import styles from "./stack-page.module.css";
+
+interface IStringStatePair {
+  string: string;
+  state: ElementStates;
+}
 
 export const StackPage: React.FC = () => {
+
+  const { values, handleChange, setValues } = useFormAndValidation();
+  const [circles, setCircles] = useState<IStringStatePair[]>([])
+
+  useEffect(()=> setValues({string:""}),[])
+
+  const setCirclesWithDelay = async (array: IStringStatePair[], ms:number = SHORT_DELAY_IN_MS) => {
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        setCircles([...array]);
+        resolve(true);
+      }, 500);
+
+    });
+
+  }
+
+    const stack = new Stack<string>();
+
+  const handleAdd = async ()=>{
+    stack.push(values['string']);
+    setValues({string:""})
+    circles.push({
+      string: stack.peak()!,
+      state: ElementStates.Changing,
+    });
+    await setCirclesWithDelay([...circles],0);
+    circles[circles.length-1].state = ElementStates.Default;
+    await setCirclesWithDelay([...circles]);
+
+  }
+
+  const handleRemove = async ()=>{
+    circles[circles.length-1].state = ElementStates.Changing;
+    setCircles([...circles]);
+    stack.pop();
+    circles.pop();
+    await setCirclesWithDelay([...circles]);
+  }
+  
+  const handleClear = ()=>{
+    stack.clear();
+    setCircles([]);
+  }
+
+
   return (
     <SolutionLayout title="Стек">
+      <div className={styles.wrapper}>
+        <form className={styles.form} onSubmit={(e)=>{e.preventDefault(); values['string'].length > 0 && handleAdd()}}>
+          <Input isLimitText maxLength={4} name='string' onChange={handleChange} value={values['string']??''} placeholder="Введите текст" />
+          <Button disabled={values['string'] === ''} onClick={handleAdd} type="button" text="Добавить" />
+          <Button disabled={circles.length==0} onClick={handleRemove} type="button" text="Удалить" />
+          <Button disabled={circles.length==0} onClick={handleClear} type="button" text="Очистить" />
+        </form>
 
+        <div className={styles.circles}>
+          {circles.map((item, index) => (
+              
+                <Circle
+                    key={index}
+                    state={item.state}
+                    letter={item.string || ''}
+                    index={index}
+                    head={circles.length - 1 === index ? 'top' : ''}
+                />
+             
+          ))}
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from './string.module.css';
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { useFormAndValidation } from "../../hooks/useForm";
+import { DELAY_IN_MS } from "../../constants/delays";
+
+interface ICharStatePair {
+    char: string;
+    state?: ElementStates;
+}
 
 export const StringComponent: React.FC = () => {
+
+    const {values, handleChange} = useFormAndValidation();
+    const [loader, setLoader] = useState(false);
+    const [circles, setCircles] = useState<ICharStatePair[]>([])
+
+    async function begin(e:React.FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+
+      const arr: ICharStatePair[] = values["string"].split('').map(item => {
+        return {
+            char: item,
+            state: ElementStates.Default
+        }
+      })
+      
+      setCircles(arr);
+      setLoader(true);
+
+
+      for (let i = 0; i < arr.length / 2; i++) {
+        const start = i;
+        const end = arr.length - 1 - start;
+
+       await select(arr, start, end)
+       .then(res => modify(res, start, end));
+      }
+
+      setLoader(false);
+      
+
+    }
+
+    async function select(array:ICharStatePair[], start: number, end:number) {
+      return new Promise<ICharStatePair[]>((resolve) => {
+
+        array[start].state = ElementStates.Changing;
+        array[end].state = ElementStates.Changing;
+        setCircles([...array]); 
+
+        setTimeout(() => 
+        {
+          resolve(array)
+        }, DELAY_IN_MS);
+      });
+    }
+
+    async function modify(array:ICharStatePair[], start: number, end:number) {
+      
+      let swapped = array[start].char;
+      array[start] = {
+          char: array[end].char,
+          state: ElementStates.Modified,
+      };
+      array[end] = {
+          char: swapped,
+          state: ElementStates.Modified,
+      }
+      setCircles([...array]);
+    }
+
   return (
     <SolutionLayout title="Строка">
-     
+        <div className={styles.wrapper}>
+
+            <form className={styles.form} onSubmit={begin}>
+                <Input name="string" value={values["string"]??""} maxLength={11} isLimitText={true} onChange={handleChange}/>
+                <Button type="submit" text="Развернуть" isLoader={loader} disabled = { values["string"]?.length < 2 }/>
+            </form>
+
+            <div className={styles.circles}>
+                {circles.map((item, i) => 
+                  <Circle key={i} letter={item.char} state={item.state}/>
+                )}
+            </div>
+
+        </div>
     </SolutionLayout>
   );
 };
